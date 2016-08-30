1b5df641975c48f5c103d561d52e8548e7585761
minchul47 <mccha47@hanmail.net>
2016-08-30 15:07:30 +0900

    RocketTrip
    Select_Interest_Activity ver.01

---
+++ RocketTrip/app/src/main/java/com/example/rockettrip/Select_Interests.java
@@ -0,0 +1,13 @@
+package com.example.rockettrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Select_Interests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_interests);
+    }
+}
