3cc1416b2ddd67a33aff6b58b471672ad5a51c16
minchul47 <mccha47@hanmail.net>
2016-09-02 13:09:23 +0900

    Start another activity of another application
    startanotheractivity_01
    startanotheractivity_03

---
+++ StartAnotherActivity_01/app/src/main/java/com/example/startanotheractivity_01/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.startanotheractivity_01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+
+    Button btnStartAnotherActivity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStartAnotherActivity = (Button) findViewById(R.id.btnStartAnotherActivity);
+
+        btnStartAnotherActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        Intent inent = new Intent("com.example.startanotheractivity_03.ANOTHER_ACTIVITY");
+
+        startActivity(inent);
+    }
+}
